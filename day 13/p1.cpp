bc11820f01dfb7416c1833aedf9bf684b019400e
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-09 14:35:09 +0530

    rescursion binary search

--- day 13/p1.cpp
+++ day 13/p1.cpp
@@ -4,7 +4,7 @@ int Binarysearch(vector<int> arr, int tar){
     int st=0;
     int end=arr.size()-1;
     while(st <= end){
-        int mid = (st + end)/2;
+        int mid = st + (end-st)/2;
         if(tar > arr[mid]){
             st = mid + 1;
 
@@ -22,6 +22,6 @@ int Binarysearch(vector<int> arr, int tar){
 
 int main(){
     vector<int> arr1={-1,0,3,4,5,9,12,23};
-    int tar1 = 12;
+    int tar1 = 100;
     cout<<Binarysearch(arr1,tar1)<<endl;
 }
---
+++ day 13/recursiveBS.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int RecBinarySea(vector<int>arr,int tar,int st,int end){
+    
+    int mid = st + (end-st)/2;
+    if(st<=end){
+
+    if(tar>arr[mid]){
+        return RecBinarySea(arr,tar,mid+1,end);
+    }
+    else if(tar < arr[mid]){
+        return RecBinarySea(arr,tar,st,mid-1);
+    }
+    else{
+        return mid;
+    }
+    }
+    return -1;
+
+}
+
+int main(){ 
+    vector<int>arr1={-1,0,3,5,9,12};
+    int target1=0;
+    cout<<RecBinarySea(arr1,target1,0,arr1.size()-1)<<endl;
+}
