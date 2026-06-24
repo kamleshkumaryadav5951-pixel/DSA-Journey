60079d566cc6a1fb12ccaf1a37ea771b5df526af
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-24 16:21:44 +0530

    2D array

---
+++ day 16/p3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<vector<int>> mat = {{1,2,3},{3,4,5},{6,7,8}};
+
+    // rows = mat.size()
+    // cols = mat[i].size()
+
+    for(int i=0 ; i<mat.size() ; i++){
+        for(int j=0  ; j<mat[i].size() ; j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<< endl;
+    }
+
+    return 0;
+}
