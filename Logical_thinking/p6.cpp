9bbe04da5c749fd6f6ac6e24868f88e1cf7d2121
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-07-23 12:46:16 +0530

    pattern

--- Logical_thinking/p6.cpp
+++ Logical_thinking/p6.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 using namespace std;
-
+int num=1;
 void pattern(int n){
-    
+    for(int i=1; i<=n; i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num +=1;
+        }
+        cout<<endl;
+    }
 }
 
 int main(){
+    pattern(5);
     return 0;
 }
