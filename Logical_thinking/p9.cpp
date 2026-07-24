2114f3300ac5edd9c9231b1a5419b2d531cf5428
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-07-24 12:24:22 +0530

    pattern

---
+++ Logical_thinking/p9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    for(int i=0; i<n; i++){
+        char ch = 'A'+i;
+        for(char j=0;j<=i;j++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+
+}
+
+int main(){
+    pattern(5);
+    return 0;
+}
